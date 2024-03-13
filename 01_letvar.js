491d523f6bdbf34c05e74ffdc54656e1b6a065db
jaimin koladiya <jaimin.koladiya@lucentinnovation.com>
2024-03-13 11:58:45 +0530

    let_var_const variable concept

---
+++ 01_letvar.js
@@ -0,0 +1,28 @@
+// var a=10;
+// var a=20;   // allowed
+// console.log(a);
+
+// let a=10;
+// let a=20;   // not allowed
+// console.log(a);
+
+// const a=10;
+// a=20;      // not possible
+// const a=20;    // not allowed
+
+// var a = 10;
+// let a = 10; //it will give an error that a has been already declared
+// console.log(a);
+
+let a = 10;
+
+if (true) {
+    // let a = 20;
+    console.log(a);
+}
+if (true) {
+    // let a = 20;
+    console.log(a);
+}
+
+// above code is print 10 and 20 both if i am trying with if and else if condition then first time it will print only 10
